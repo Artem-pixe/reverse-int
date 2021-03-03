f769afcc79abd372585c27838953a419c7d6ddf5
Artem <artemmz@inbox.ru>
2021-03-03 18:05:36 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+        n = String(n).split("").reverse();
+        n = Number(n.join(""));
+
+        return n;
+    } else  { 
+        n = String(n).split("").reverse();
+        n = Number(n.join(""));
+
+        return n;
+    }
 }
+
